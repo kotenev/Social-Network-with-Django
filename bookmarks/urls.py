b7d36d696fe07f20e29f2ff63a2b498395b7b142
faizalazman <faizalazman@chatoyance.org>
2018-08-07 21:52:01 +0800

    commit

--- bookmarks/urls.py
+++ bookmarks/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/',include('account.urls')),
-    path('shop/',include('shop.urls')),
+    path('shop/',include('shop.urls',namespace= 'shop')),
 ]
 
 if settings.DEBUG:
